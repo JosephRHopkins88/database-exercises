85f3561e5cc6da684bf1c6f00397b70c73f1e0ce
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-06-17 16:33:29 -0500

    complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,13 +1,13 @@
-USE
-codeup_test_db;
+USE codeup_test_db;
+
 DROP TABLE IF EXISTS albums;
-CREATE TABLE albums
-(
+
+CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name VARCHAR(50) NOT NULL,
     release_date INT NOT NULL,
-    sales DOUBLE NOT NULL,
+    sales DECIMAL (15, 2),
     genre VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
