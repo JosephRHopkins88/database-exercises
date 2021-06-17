747e824cf07d33b898d38c09169fb8e3833f1bb3
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-06-17 15:32:55 -0500

    test

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,13 @@
-USE codeup_test_db;
-
-CREATE TABLE quotes (
-                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist VARCHAR(50) DEFAULT 'NONE',
-                        name  VARCHAR(100) DEFAULT 'NONE',
-                        release_date DATE,
-                        sales INT UNSIGNED,
-                        genre CHAR(100) DEFAULT 'NONE',
-                        PRIMARY KEY (id)
+USE
+codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    release_date INT NOT NULL,
+    sales DOUBLE NOT NULL,
+    genre VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
