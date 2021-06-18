c4da8c7d9de3ac23f5d5710d41df1ba061582930
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-06-18 10:35:43 -0500

    update

---
+++ albums_seeder.sql
@@ -0,0 +1,7 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles',          'Their Greatest Hits', 1976, 41.2, 'Country Rock'),
+       ('Whitney Houston', 'The Bodyguard',       1992, 32.4, 'R&B'),
+       ('Eagles',          'Hotel California',    1976, 31.5, 'Soft Rock'),
+       ('Michael Jackson', 'Thriller',            1982, 47.3, 'Pop');
