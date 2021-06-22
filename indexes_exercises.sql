9522a06287e5e64f9bebad5c27f847fc3d49d3d8
Joseph Hopkins <joseph.r.hopkins88@gmail.com>
2021-06-22 15:08:41 -0500

    exercise complete

---
+++ indexes_exercises.sql
@@ -0,0 +1,22 @@
+USE employees;
+
+DESCRIBE salaries;
+
+DESCRIBE employees;
+
+DESCRIBE dept_manager;
+
+DESCRIBE dept_emp;
+
+DESCRIBE departments;
+
+DESCRIBE titles;
+
+USE codeup_test_db;
+
+DESCRIBE albums;
+
+DESCRIBE quotes;
+
+ALTER TABLE albums ADD UNIQUE  unique_artist_and_name (artist, name);
+
